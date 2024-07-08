6aa3d8f23abfb5065ab740e7020c85b59be8d695
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2024-07-08 12:07:35 +0000

    Bug-fix: unbreak the 'test' target within program/library directories.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@4044 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/elftoolchain.test-target.mk
+++ mk/elftoolchain.test-target.mk
@@ -4,13 +4,16 @@
 # Rules for invoking test suites.
 #
 
-TEST_DIRECTORY=		test
+TEST_DIRECTORY=		tests
 TEST_TARGET=		test
 
-.if !target(test)
+TEST_FRAMEWORK?=	tet	# Or: 'atf', 'custom' or 'libtest'.
+
+.if !target(${TEST_TARGET})
 # The special target 'test' runs the test suite associated with a
 # utility or library.
 test:	all .PHONY
-	cd ${TOP}/${TEST_DIRECTORY}/${.CURDIR:T} && \
-	${MAKE} all ${TEST_TARGET}
+	(cd ${TOP}/${TEST_DIRECTORY}/${TEST_FRAMEWORK}/${.CURDIR:T} && \
+	${MAKE} all && \
+	${MAKE} ${TEST_TARGET})
 .endif
