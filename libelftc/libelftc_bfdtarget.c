bc9222a3c7dd3e0bf7a7a7d70e70d07cec09b0b6
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2025-02-12 15:48:52 +0000

    libelftc: Keep a list of BFD target descriptors sorted by name.
    While here, neaten whitespace use.
    
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@4182 95820547-d848-0410-985e-9ae8fe0fa350

--- libelftc/libelftc_bfdtarget.c
+++ libelftc/libelftc_bfdtarget.c
@@ -33,10 +33,21 @@
 ELFTC_VCSID("$Id$");
 
 struct _Elftc_Bfd_Target _libelftc_targets[] = {
+	{
+		.bt_name      = "binary",
+		.bt_type      = ETF_BINARY,
+	},
 
 	{
-		.bt_name = "binary",
-		.bt_type = ETF_BINARY,
+		.bt_name      = "efi-app-ia32",
+		.bt_type      = ETF_EFI,
+		.bt_machine   = EM_386,
+	},
+
+	{
+		.bt_name      = "efi-app-x86_64",
+		.bt_type      = ETF_EFI,
+		.bt_machine   = EM_X86_64,
 	},
 
 	{
@@ -127,23 +138,6 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
-		.bt_name      = "elf64-loongarch",
-		.bt_type      = ETF_ELF,
-		.bt_byteorder = ELFDATA2LSB,
-		.bt_elfclass  = ELFCLASS64,
-		.bt_machine   = EM_LOONGARCH,
-	},
-
-	{
-		.bt_name      = "elf64-loongarch-freebsd",
-		.bt_type      = ETF_ELF,
-		.bt_byteorder = ELFDATA2LSB,
-		.bt_elfclass  = ELFCLASS64,
-		.bt_machine   = EM_LOONGARCH,
-		.bt_osabi     = ELFOSABI_FREEBSD,
-	},
-
-	{
 		.bt_name      = "elf32-powerpc",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2MSB,
@@ -169,6 +163,14 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
+		.bt_name      = "elf32-riscv",
+		.bt_type      = ETF_ELF,
+		.bt_byteorder = ELFDATA2LSB,
+		.bt_elfclass  = ELFCLASS32,
+		.bt_machine   = EM_RISCV,
+	},
+
+	{
 		.bt_name      = "elf32-sh",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2MSB,
@@ -177,26 +179,18 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
-		.bt_name      = "elf32-shl",
+		.bt_name      = "elf32-sh-linux",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS32,
 		.bt_machine   = EM_SH,
+		.bt_osabi     = ELFOSABI_LINUX,
 	},
 
 	{
 		.bt_name      = "elf32-sh-nbsd",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2MSB,
-		.bt_elfclass  = ELFCLASS32,
-		.bt_machine   = EM_SH,
-		.bt_osabi     = ELFOSABI_NETBSD,
-	},
-
-	{
-		.bt_name      = "elf32-shl-nbsd",
-		.bt_type      = ETF_ELF,
-		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS32,
 		.bt_machine   = EM_SH,
 		.bt_osabi     = ELFOSABI_NETBSD,
@@ -212,12 +206,20 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
-		.bt_name      = "elf32-sh-linux",
+		.bt_name      = "elf32-shl",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS32,
 		.bt_machine   = EM_SH,
-		.bt_osabi     = ELFOSABI_LINUX,
+	},
+
+	{
+		.bt_name      = "elf32-shl-nbsd",
+		.bt_type      = ETF_ELF,
+		.bt_byteorder = ELFDATA2LSB,
+		.bt_elfclass  = ELFCLASS32,
+		.bt_machine   = EM_SH,
+		.bt_osabi     = ELFOSABI_NETBSD,
 	},
 
 	{
@@ -316,6 +318,23 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
+		.bt_name      = "elf64-loongarch",
+		.bt_type      = ETF_ELF,
+		.bt_byteorder = ELFDATA2LSB,
+		.bt_elfclass  = ELFCLASS64,
+		.bt_machine   = EM_LOONGARCH,
+	},
+
+	{
+		.bt_name      = "elf64-loongarch-freebsd",
+		.bt_type      = ETF_ELF,
+		.bt_byteorder = ELFDATA2LSB,
+		.bt_elfclass  = ELFCLASS64,
+		.bt_machine   = EM_LOONGARCH,
+		.bt_osabi     = ELFOSABI_FREEBSD,
+	},
+
+	{
 		.bt_name      = "elf64-powerpc",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2MSB,
@@ -338,14 +357,6 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS64,
 		.bt_machine   = EM_PPC64,
-	},
-
-	{
-		.bt_name      = "elf32-riscv",
-		.bt_type      = ETF_ELF,
-		.bt_byteorder = ELFDATA2LSB,
-		.bt_elfclass  = ELFCLASS32,
-		.bt_machine   = EM_RISCV,
 	},
 
 	{
@@ -374,26 +385,18 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
-		.bt_name      = "elf64-sh64l",
+		.bt_name      = "elf64-sh64-linux",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS64,
 		.bt_machine   = EM_SH,
+		.bt_osabi     = ELFOSABI_LINUX,
 	},
 
 	{
 		.bt_name      = "elf64-sh64-nbsd",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2MSB,
-		.bt_elfclass  = ELFCLASS64,
-		.bt_machine   = EM_SH,
-		.bt_osabi     = ELFOSABI_NETBSD,
-	},
-
-	{
-		.bt_name      = "elf64-sh64l-nbsd",
-		.bt_type      = ETF_ELF,
-		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS64,
 		.bt_machine   = EM_SH,
 		.bt_osabi     = ELFOSABI_NETBSD,
@@ -409,12 +412,20 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
-		.bt_name      = "elf64-sh64-linux",
+		.bt_name      = "elf64-sh64l",
 		.bt_type      = ETF_ELF,
 		.bt_byteorder = ELFDATA2LSB,
 		.bt_elfclass  = ELFCLASS64,
 		.bt_machine   = EM_SH,
-		.bt_osabi     = ELFOSABI_LINUX,
+	},
+
+	{
+		.bt_name      = "elf64-sh64l-nbsd",
+		.bt_type      = ETF_ELF,
+		.bt_byteorder = ELFDATA2LSB,
+		.bt_elfclass  = ELFCLASS64,
+		.bt_machine   = EM_SH,
+		.bt_osabi     = ELFOSABI_NETBSD,
 	},
 
 	{
@@ -468,46 +479,34 @@ struct _Elftc_Bfd_Target _libelftc_targets[] = {
 	},
 
 	{
-		.bt_name = "ihex",
-		.bt_type = ETF_IHEX,
+		.bt_name      = "ihex",
+		.bt_type      = ETF_IHEX,
 	},
 
 	{
-		.bt_name = "srec",
-		.bt_type = ETF_SREC,
+		.bt_name      = "pei-i386",
+		.bt_type      = ETF_PE,
+		.bt_machine   = EM_386,
 	},
 
 	{
-		.bt_name = "symbolsrec",
-		.bt_type = ETF_SREC,
+		.bt_name      = "pei-x86-64",
+		.bt_type      = ETF_PE,
+		.bt_machine   = EM_X86_64,
 	},
 
 	{
-		.bt_name    = "efi-app-ia32",
-		.bt_type    = ETF_EFI,
-		.bt_machine = EM_386,
+		.bt_name      = "srec",
+		.bt_type      = ETF_SREC,
 	},
 
 	{
-		.bt_name    = "efi-app-x86_64",
-		.bt_type    = ETF_EFI,
-		.bt_machine = EM_X86_64,
+		.bt_name      = "symbolsrec",
+		.bt_type      = ETF_SREC,
 	},
 
 	{
-		.bt_name    = "pei-i386",
-		.bt_type    = ETF_PE,
-		.bt_machine = EM_386,
-	},
-
-	{
-		.bt_name    = "pei-x86-64",
-		.bt_type    = ETF_PE,
-		.bt_machine = EM_X86_64,
-	},
-
-	{
-		.bt_name = NULL,
-		.bt_type = ETF_NONE,
+		.bt_name      = NULL,
+		.bt_type      = ETF_NONE,
 	},
 };
