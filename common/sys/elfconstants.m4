dnelson-1901/elftoolchain

commit a45a14ee6727e96500bbdf2a04961c9bf3414bce

elfconstants: Add constants for the Alpha architecture.
git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@4282 95820547-d848-0410-985e-9ae8fe0fa350

common/sys/elfconstants.m4
@@ -51,6 +51,9 @@ define(`COMPATIBILITY_NOTICE',`dnl
 #     ELF for the Arm® Architecture
 #     https://github.com/ARM-software/abi-aa/blob/main/aaelf32/aaelf32.rst
 #
+#   alpha ::
+#     Believed to be compatible with NetBSD/Alpha and GNU binutils.
+#
 #   ia_64 ::
 #      Intel® Itanium™ Processor-specific Application Binary Interface (ABI)
 #      Document Number: 245370-003
@@ -374,6 +377,8 @@ _(`DT_VERNEEDNUM',       0x6FFFFFFF,
 	`the number of version needed entries')
 _(`DT_LOPROC',           0x70000000,
 	`start of processor-specific types')
+_(`DT_ALPHA_PLTRO',      0xx70000000,
+	`secure (read-only) PLT')
 _(`DT_ARM_SYMTABSZ',     0x70000001,
 	`number of entries in the dynamic symbol table')
 _(`DT_SPARC_REGISTER',   0x70000001,
@@ -2432,6 +2437,89 @@ _(R_ARM_THM_PC11,		102)
 _(R_ARM_THM_PC9,		103)
 ')
 
+define(`DEFINE_ALPHA_RELOCATION_TYPES',`
+_(R_ALPHA_NONE,			0,
+	`No relocation')
+_(R_ALPHA_REFLONG,		1,
+	`32 bit direct')
+_(R_ALPHA_REFQUAD,		2,
+	`64 bit direct')
+_(R_ALPHA_GPREL32,		3,
+	`GP-relative 32-bit')
+_(R_ALPHA_LITERAL,		4,
+	`GP-relative 16-bit')
+_(R_ALPHA_LITUSE,		5,
+	`Optimization hint for LITERAL')
+_(R_ALPHA_GPDISP,		6,
+	`Add displacement to GP')
+_(R_ALPHA_BRADDR,		7,
+	`PC+4-relative 23-bit shifted')
+_(R_ALPHA_HINT,		        8,
+	`PC+4-relative 16-bit shifted')
+_(R_ALPHA_SREL16,		9,
+	`PC-relative 16 bit')
+_(R_ALPHA_SREL32,		10,
+	`PC-relative 32 bit')
+_(R_ALPHA_SREL64,		11,
+	`PC-relative 64 bit')
+_(R_ALPHA_OP_PUSH,		12,
+	`deprecated, ECOFF OP stack push')
+_(R_ALPHA_OP_STORE,		13,
+	`deprecated, ECOFF OP pop and store')
+_(R_ALPHA_OP_PSUB,		14,
+	`deprecated, ECOFF OP stack subtract')
+_(R_ALPHA_OP_PRSHIFT,		15,
+	`deprecated, ECOFF OP stack right `shift'')
+_(R_ALPHA_GPVALUE,   	        16,
+	`deprecated, ECOFF relocation')
+_(R_ALPHA_GPRELHIGH,		17,
+	`GP-relative 32-bit high 16 bits')
+_(R_ALPHA_GPRELLOW,  	    	18,
+	`GP-relative 32-bit low 16 bits')
+_(R_ALPHA_GPREL16,   	    	19,
+	`GP-relative 16-bit')
+_(R_ALPHA_IMMED_GP_HI32,	20,
+	`deprecated ECOFF relocation')
+_(R_ALPHA_IMMED_SCN_HI32,	21,
+	`deprecated ECOFF relocation')
+_(R_ALPHA_IMMED_BR_HI32,	22,
+	`deprecated ECOFF relocation')
+_(R_ALPHA_IMMED_LO32,		23,
+	`deprecated ECOFF relocation')
+__(`	', `Relocations for shared libraries')	
+_(R_ALPHA_COPY,			24,
+	`copy symbol at runtime')
+_(R_ALPHA_GLOB_DAT,		25,
+	`create GOT entry')
+_(R_ALPHA_JMP_SLOT,		26,
+	`create PLT entry')
+_(R_ALPHA_RELATIVE,		27,
+	`adjust by program base')
+_(R_ALPHA_BRSGP,		28,
+	`PC relative with target address adjustment')
+__(`	', `TLS relocations')
+_(R_ALPHA_TLSGD,     	  	29)
+_(R_ALPHA_TLSDM,		30)
+_(R_ALPHA_DTPMOD64,		31)
+_(R_ALPHA_GOTDTPREL,		32)
+_(R_ALPHA_DTPREL64,		33)
+_(R_ALPHA_DTPRELHI,		34)
+_(R_ALPHA_DTPRELLO,		35)
+_(R_ALPHA_DTPREL16,		36)
+_(R_ALPHA_GOTTPREL,		37)
+_(R_ALPHA_TPREL64,		38)
+_(R_ALPHA_TPRELHI,		39)
+_(R_ALPHA_TPRELLO,		40)
+_(R_ALPHA_TPREL16,		41)
+')
+
+define(`DEFINE_ALPHA_RELOCATION_TYPE_SYNONYMS',`
+_(R_ALPHA_TLS_GD,		R_ALPHA_TLSGD,
+	`NetBSD spelling')
+_(R_ALPHA_IMMED_GP_16,		R_ALPHA_GPREL16,
+	`NetBSD spelling')
+')
+
 define(`DEFINE_IA_64_RELOCATION_TYPES',`
 __(`EM_IA_64')
 _(R_IA_64_NONE,			0)
@@ -3494,6 +3582,7 @@ define(`DEFINE_RELOCATION_TYPES',`
 DEFINE_386_RELOCATION_TYPES()
 DEFINE_AARCH64_RELOCATION_TYPES()
 DEFINE_ARM_RELOCATION_TYPES()
+DEFINE_ALPHA_RELOCATION_TYPES()
 DEFINE_IA_64_RELOCATION_TYPES()
 DEFINE_LOONGARCH_RELOCATION_TYPES()
 DEFINE_MIPS_RELOCATION_TYPES()
@@ -3519,6 +3608,7 @@ DEFINE_X86_64_OBSOLETE_RELOCATION_TYPES()
 define(`DEFINE_RELOCATION_TYPE_SYNONYMS',`
 DEFINE_386_RELOCATION_TYPE_SYNONYMS()
 DEFINE_AARCH64_RELOCATION_TYPE_SYNONYMS()
+DEFINE_ALPHA_RELOCATION_TYPE_SYNONYMS()
 DEFINE_IA_64_RELOCATION_TYPE_SYNONYMS()
 DEFINE_MIPS_RELOCATION_TYPE_SYNONYMS()
 DEFINE_X86_64_RELOCATION_TYPE_SYNONYMS()

commit 7bdfb6c7aec1402dc05cfba3ac75f34906bed002

elfconstants: Fix a typo introduced in [r4282].
git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@4283 95820547-d848-0410-985e-9ae8fe0fa350

common/sys/elfconstants.m4
@@ -377,7 +377,7 @@ _(`DT_VERNEEDNUM',       0x6FFFFFFF,
 	`the number of version needed entries')
 _(`DT_LOPROC',           0x70000000,
 	`start of processor-specific types')
-_(`DT_ALPHA_PLTRO',      0xx70000000,
+_(`DT_ALPHA_PLTRO',      0x70000000,
 	`secure (read-only) PLT')
 _(`DT_ARM_SYMTABSZ',     0x70000001,
 	`number of entries in the dynamic symbol table')