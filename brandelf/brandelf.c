4426625b4bc32234b1cdb3282c9a3fcc65c619d1
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2026-02-26 12:50:47 +0000

    brandelf: Use elftc_get_program_name(3).
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@4337 95820547-d848-0410-985e-9ae8fe0fa350

--- brandelf/brandelf.c
+++ brandelf/brandelf.c
@@ -261,14 +261,14 @@ Usage: %s [options] file...\n\
 static void
 usage(int exit_code)
 {
-	(void) fprintf(stderr, USAGE_MESSAGE, ELFTC_GETPROGNAME());
+	(void) fprintf(stderr, USAGE_MESSAGE, elftc_get_program_name());
 	exit(exit_code);
 }
 
 static void
 printversion(void)
 {
-	(void) printf("%s (%s)\n", ELFTC_GETPROGNAME(), elftc_version());
+	(void) printf("%s (%s)\n", elftc_get_program_name(), elftc_version());
 	exit(EX_OK);
 }
 
